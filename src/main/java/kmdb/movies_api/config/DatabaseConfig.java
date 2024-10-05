ae3f09ad3dc8a53f03c8d9d8f6e024287267b2ab
Andre Berezin <andreberezin@outlook.com>
2024-10-05 18:03:42 +0300

    fixed config error

--- src/main/java/kmdb/movies_api/config/DatabaseConfig.java
+++ src/main/java/kmdb/movies_api/config/DatabaseConfig.java
@@ -1,6 +1,5 @@
 package kmdb.movies_api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -12,13 +11,8 @@ import java.util.Objects;
 @Configuration
 public class DatabaseConfig {
 
-    //@Autowired
-    //private Environment env;
-
     private final Environment env;
 
-    // Constructor-based injection
-    @Autowired
     public DatabaseConfig(Environment env) {
         this.env = env;
     }
@@ -26,11 +20,10 @@ public class DatabaseConfig {
     @Bean
     public DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        //dataSource.setDriverClassName(env.getProperty("driverClassName"));
-        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("driverClassName")));
-        dataSource.setUrl(env.getProperty("url"));
-        dataSource.setUsername(env.getProperty("user"));
-        dataSource.setPassword(env.getProperty("password"));
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.driver-class-name")));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
         return dataSource;
     }
 }
